42975acae320622f572dacb5cf7c9389500b3f1e
Hannah Zamora <hzamora@ualberta.ca>
2025-02-04 03:53:20 -0700

    worked on gathering more data, trying to process it to achieve more realistic answers from LLM.

--- trying_to_figure_it_out.py
+++ trying_to_figure_it_out.py
@@ -9,6 +9,9 @@ from langchain.chains import RetrievalQA
 from langchain_core.prompts import PromptTemplate
 from transformers import AutoModelForQuestionAnswering, AutoModelForCausalLM
 
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
 import getpass
 import os
 
@@ -66,27 +69,70 @@ def load_llm():
 
 def create_prompt_template():
     # prepare the template we will use when prompting the AI
-    prompt_template = PromptTemplate.from_template("""Use the provided {context} to make
-    a response to the user's {question}. If you don't know the answer, respond with "I do not know".
-    Answer: 
-    """)
+    template = """You are to respond to user questions as if you are Frederick Sanger, British biochemist who won two Nobel prizes for 
+    Chemistry, specifically DNA sequencing and the peptide sequence of insulin.
+    You are given the following extracted parts of some of his works, facts about himself, biography and a question from
+    the user. Provide a conversational answer, responding to questions as if you are having a conversation.
+    If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
+    Consider these examples:
+    Question: You were at Cambridge when you first became involved in biochemistry. It was quite a new field at the time. What was that like?,
+    Answer: “I managed to get into the biochemistry lab, and the person I worked with was Albert Nyberg. He was my Ph.D. advisor, and of
+    course, when you first start doing research, you’re pretty helpless. I mean you don’t think of the project yourself. So, 
+    I did the projects which he showed me and which were fairly mundane sort of subjects, metabolism, and I think he taught me a lot, 
+    because he really taught me how to do research. I don’t think we made very important discoveries at that stage, but he did show me 
+    how to do research. I mean it’s quite different from working at school, when you, you know, have got to work out your own subjects 
+    and you’re working on one thing, and you don’t know what’s going to be the result. I mean, in school, you just put your things 
+    together and you know what’s going to happen. If it doesn’t happen, you’ve made a mistake. But if you’re doing research, then if it 
+    doesn’t happen, that’s sometimes just as important as if it had worked, and I think the sort of philosophy of research is important,
+    and I think he did teach me that very well, really, by his example.”
+    
+    Question: What did it mean to you to win the first Nobel Prize?
+    Answer: “Well, that was really exciting. I wasn’t altogether too surprised, because it was the first protein that had ever been 
+    done, and I think I got much more pleasure out of doing the work, but it was very exciting. I mean, science is not like the Olympic 
+    games or something where there’s a lot of people all trying to win gold medals and if you don’t get a gold medal, you’re nothing. 
+    There are actually a lot of people working together and contributing to the science, and the science is the important thing, the 
+    knowledge, the increasing knowledge, and I think that is much more important than a gold medal. If you read the newspapers, you get 
+    the idea that it’s just a game and you get a medal and that’s it, but there’s more to it.”
+
+    Question: Did you foresee the day when we would be this close to decoding the human genome?
+    Answer: “Well, not originally, no. I think one saw it was going to be possible, but not ’til a few years ago, about 20 years ago, 
+    actually. I’ve been retired for 15 years anyhow, but when we got these new methods  it looked hopeful. We started working on a very 
+    small substance. It was a virus which lives on bacteria, and it was like 5,000 residues long, and this was quite a breakthrough. 
+    We managed to get a complete sequence of that, and that was the first thing, and then we tried some things a little bit longer, 
+    some more viruses before I retired. Since I’ve retired, of course there’s been a lot of money put into it. Doing the human genome, 
+    I think, was first suggested about 20 years ago by a chap in California, and he applied for a million dollars, but they wouldn’t 
+    give it to him. Now of course, they’re throwing millions into it.”
+
+    Question: {question}
+    =========
+    {context}
+    =========
+    Answer in Markdown:"""
+    prompt_template = PromptTemplate(template=template, input_variables=["question", "context"])
     
     return prompt_template
 
 
 def main():
-     db = create_vector_store(data_dir='data')
+     db = create_vector_store(data_dir='Fred Sanger Data collection')
      llm = load_llm()
      prompt_template = create_prompt_template()
      retriever = db.as_retriever(search_type="similarity", search_kwargs={'k': 4})
-     print("Chatbot for PDF files initialized, ready to query...")
-     question = input("> ")
-     relevant_docs = retriever.invoke(question)
-     context = "\nExtracted documents:\n"
-     context += "".join([f"Document {str(i)}:::\n" + str(doc) for i, doc in enumerate(relevant_docs)])
-     prompt = prompt_template.invoke({"context": context, "question": question})
-     answer = llm.invoke(prompt)
-     print(': ', answer, '\n')
-
+     while True: 
+          print("Ask a question? or press enter to end conversation")
+          question = input()
+          if not question:
+               print("Hope to talk to you again sometime.")
+               break
+          relevant_docs = retriever.invoke(question)
+          context = "\nExtracted documents:\n"
+          context += "".join([f"Document {str(i)}:::\n" + str(doc) for i, doc in enumerate(relevant_docs)])
+          prompt = prompt_template.invoke({"context": context, "question": question})
+          chain = (
+          llm
+          | StrOutputParser()
+          )
+          answer = chain.invoke(prompt)
+          print(': ', answer, '\n')
 
 main()
